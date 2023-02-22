a4619b5d30378ca78d79dedcb0b455d5a0ad4c81
Andrei <nenaxov082008@mail.ru>
2023-02-22 19:20:42 +0300

    Задача 2

---
+++ Задача 2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите два числа : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+   
+    if (a > b)
+    
+{
+    
+Console.WriteLine("{0} наибольшее число", a);
+    
+}
+
+    if (a < b)
+{
+    
+Console.WriteLine("{0} наибольшее число ", b);
+    
+}
+Console.ReadLine();
+
