68e7cbe608d28273a1c8b99541f9b6d902a8c9e3
Andrei <nenaxov082008@mail.ru>
2023-03-16 13:32:31 +0300

    Задача 64

--- Задача 64/Program.cs
+++ Задача 64/Program.cs
@@ -3,21 +3,33 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Write("Введите число: ");
-int = = Convert.ToInt32(Console.ReadLine());
-1  = = 1;
-NaturalToLow(number, count);
+// Console.Write("Введите число: ");
+// int = Convert.ToInt32(Console.ReadLine());
+
+// NaturalToLow(number, count);
 
 
-void NaturalToLow(int n, int count)
+// void NaturalToLow(int n, int count)
+// {
+//     if (count > n)
+//     {
+//         return;
+//     }
+//     else
+//     {
+//         NaturalToLow(n, count  + 1);
+//         Console.Write (count  + " ");
+//     }
+// }
+Console.WriteLine("Введите натуральное число больше 1:");
+int = int.Parse(Console.ReadLine());
+
+void NumberCounter (int )
 {
-    if (count > n)
-    {
-        return;
-    }
-    else
-    {
-        NaturalToLow(n, count  + 1);
-        Console.Write (count  + " ");
-    }
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number  == 0) return;
+    Console.Write("{0,4}", number);
+    NumberCounter (number - 1);
 }
+
+NumberCounter(number);
