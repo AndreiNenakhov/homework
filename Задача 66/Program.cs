56771539d7a178e3091461f7d93938e27648a555
Andrei <nenaxov082008@mail.ru>
2023-03-17 11:47:50 +0300

    Задача 66

--- Задача 66/Program.cs
+++ Задача 66/Program.cs
@@ -1,23 +1,32 @@
-﻿// Задача 66: Задайте значения M и N. Напишите программу, 
-// которая найдёт сумму натуральных элементов в промежутке от M до N.
-// M = 1; N = 15 -> 120
-// M = 4; N = 8. -> 30
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
 
-Console.Write( "Введите число М:  ");
-int М = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
 
-Console.Write ( "Введите число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
-void GapnumberSum (int numberM, int numberN, int sum)
+SumFromMToN(m, n);
+
+void SumFromMToN(int m, int n)
 {
-    if (numberM < numberN)
-    {
-        Console.WriteLine ($"Сумма натуральных элементов в промежутке от М до N: {sum} " );
-        return;
-    }
-    + = = + (numberM ++);
-    GapnumberSum (numberM,numberN,sum);
+    Console.Write(SumMN(m - 1, n));
 }
 
-GapnumberSum (numberM, numberN, 0);
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
