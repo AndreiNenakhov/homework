57ae07eccf345e1190558846ddca330a66828f32
Andrei <nenaxov082008@mail.ru>
2023-03-17 11:43:40 +0300

    Задача 64

--- Задача 64/Program.cs
+++ Задача 64/Program.cs
@@ -1,35 +1,26 @@
-﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
-// Выполнить с помощью рекурсии.
-// N = 5 -> "5, 4, 3, 2, 1"
-// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
-
-// Console.Write("Введите число: ");
-// int = Convert.ToInt32(Console.ReadLine());
-
-// NaturalToLow(number, count);
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
 
 
-// void NaturalToLow(int n, int count)
-// {
-//     if (count > n)
-//     {
-//         return;
-//     }
-//     else
-//     {
-//         NaturalToLow(n, count  + 1);
-//         Console.Write (count  + " ");
-//     }
-// }
-Console.WriteLine("Введите натуральное число больше 1:");
-int = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
 
-void NumberCounter (int )
+
+void NaturalToLow(int n, int count)
 {
-    if (number < 0) Console.Write($"{number} не натуральное число");
-    if (number  == 0) return;
-    Console.Write("{0,4}", number);
-    NumberCounter (number - 1);
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
 }
-
-NumberCounter(number);
